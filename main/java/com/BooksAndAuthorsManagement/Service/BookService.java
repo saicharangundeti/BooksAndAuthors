c4e39cfee528a0fadb6b08a3b3cd564eb08a1078
Sai Charan <saicharangundeti19@gmail.com>
2024-05-01 14:45:28 +0530

    bookService is added

--- main/java/com/BooksAndAuthorsManagement/Service/BookService.java
+++ main/java/com/BooksAndAuthorsManagement/Service/BookService.java
@@ -1,4 +1,55 @@
 package com.BooksAndAuthorsManagement.Service;
 
+import com.BooksAndAuthorsManagement.model.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class BookService {
+    static Map<String, Book> bookMap = new HashMap<>();
+    public ArrayList<Book> findAllBooks(){
+        return new ArrayList<>(bookMap.values());
+    }
+    public ArrayList<Book> findAllBooksbyName(String name){
+        ArrayList<Book> specBooks = new ArrayList<>();
+        for(Book book : bookMap.values()){
+            if(book.getBookName().equals(name)){
+                specBooks.add(book);
+            }
+        }
+        return specBooks;
+    }
+    public Book findBookById(String id){
+        if(bookMap.containsKey(id)){
+            return bookMap.get(id);
+        }
+        return null;
+    }
+    public Book saveBook(Book book){
+        if(bookMap.containsKey(book.getBookId())){
+            return null;
+        }
+        bookMap.put(book.getBookId(),book);
+        return book;
+    }
+    public Book updateBook(Book book){
+        if(bookMap.containsKey(book.getBookId())){
+            Book updateBook = bookMap.get(book.getBookId());
+            updateBook.setBookName(book.getBookName());
+            updateBook.setNumberOfPages(book.getNumberOfPages());
+            updateBook.setAuthorId(book.getAuthorId());
+            return updateBook;
+        }
+        return null;
+    }
+    public boolean removeBook(String id){
+        if(bookMap.containsKey(id)){
+            bookMap.remove(id);
+            return true;
+        }
+        return false;
+    }
 }
--- main/java/com/BooksAndAuthorsManagement/controller/BookController.java
+++ main/java/com/BooksAndAuthorsManagement/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.BooksAndAuthorsManagement.controller;
 
+import com.BooksAndAuthorsManagement.Service.BookService;
 import com.BooksAndAuthorsManagement.model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,58 +12,44 @@ import java.util.*;
 @RequestMapping("/v1/books")
 
 public class BookController {
-    static Map<String, Book> bookMap = new HashMap<>();
+
+    static BookService bookService = new BookService();
+    @Autowired
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
 
     @GetMapping("/{bookId}")
     public ResponseEntity<Book> getABook(@PathVariable String bookId){
-        if(bookMap.containsKey(bookId)){
-            return ResponseEntity.ok(bookMap.get(bookId));
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        if(bookService.findBookById(bookId) == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        return ResponseEntity.ok(bookService.findBookById(bookId));
     }
 
     @GetMapping
     public static ResponseEntity<ArrayList<Book>> getAllBooks(@RequestParam(value = "bookName", required = false) String bookName) {
         if (bookName == null || bookName.equals("")) {
-            ArrayList<Book> allBooks = new ArrayList<>(bookMap.values());
-            return ResponseEntity.ok(allBooks);
-        } else {
-            ArrayList<Book> specificBooks = new ArrayList<>();
-            for (Book book : bookMap.values()) {
-                if (book.getBookName().equals(bookName)) {
-                    specificBooks.add(book);
-                }
-            }
-            if (specificBooks.size() > 0) return ResponseEntity.ok(specificBooks);
+            return ResponseEntity.ok(bookService.findAllBooks());
+        } else if(bookService.findAllBooksbyName(bookName).size() > 0){
+            return ResponseEntity.ok(bookService.findAllBooksbyName(bookName));
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
     @PostMapping
     public ResponseEntity<Book> postBook(@RequestBody Book book){
-        if(book.getBookId() == null || bookMap.containsKey(book.getBookId())){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        bookMap.put(book.getBookId(),book);
-        //check for author validations
+        if(bookService.saveBook(book) == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         return ResponseEntity.ok(book);
     }
     @PutMapping("/{bookId}")
     public ResponseEntity<Book> updateBook(@RequestBody Book book){
-        if(bookMap.containsKey(book.getBookId())){
-            Book update = bookMap.get(book.getBookId());
-            update.setBookName(book.getBookName());
-            update.setAuthorId(book.getAuthorId());
-            update.setNumberOfPages(book.getNumberOfPages());
-            bookMap.put(book.getBookId(),update);
-            return ResponseEntity.ok(update);
+        if(bookService.updateBook(book) == null ) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-
+        return ResponseEntity.ok(bookService.updateBook(book));
     }
     @DeleteMapping("/{bookId}")
     public ResponseEntity<Boolean> deleteBook(@PathVariable String bookId){
-        if(bookMap.containsKey(bookId)){
-            bookMap.remove(bookId);
+        if(bookService.removeBook(bookId) == true){
             return ResponseEntity.ok(true);
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(false);
